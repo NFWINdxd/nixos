09246fc72593b8075d95c5418c926531810fc677
NFWINdxd <noelwinsell@gmail.com>
2026-08-12 19:51:34 +0200

    init :

--- hyprland.lua
+++ hyprland.lua
@@ -23,7 +23,7 @@
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
+    output   = "eDP-2",
     mode     = "preferred",
     position = "auto",
     scale    = "auto",
